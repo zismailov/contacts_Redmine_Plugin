f17b2b9a2f4818053003238214093f9b078b16a5
Zulkar <bedelbaev@gmail.com>
2018-04-25 18:36:17 +0600

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :contacts do
+  name 'Contacts plugin'
+  author 'Zulkar Bedelbaev'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/zismailov/contacts_Redmine_Plugin'
+  author_url 'https://github.com/zismailov'
+end
